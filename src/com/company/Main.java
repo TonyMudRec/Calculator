beaf50b85094d730c6c0c762011646389a802ac4
TonyMudRec <sm1le88man@gmail.com>
2022-05-15 13:48:40 +0300

    update devide, remove isZero

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,13 +6,8 @@ public class Main {
         Calculator calc = Calculator.instance.get();
         int a = calc.plus.apply(1, 2);
         int b = calc.minus.apply(1, 1);
-        if (calc.isNotZero.test(b)) {
-            int c = calc.devide.apply(a, b);
-            calc.println.accept(c);
-        } else {
-            System.out.println("на ноль нельзя делить!");
-        }
-
+        int c = calc.devide.apply(a, b);
+        calc.println.accept(c);
 
     }
 }
